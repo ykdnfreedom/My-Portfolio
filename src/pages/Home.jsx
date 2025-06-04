ed01962c8e17fa8ed62450245ab816313e1436e7
Freed091 <freshiejunior4@gmail.com>
2025-06-03 19:06:33 -0700

    ThemeToogle

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 export const Home = () => {
     return(
 
-     <div className="min-h-screen"> 
+     <div className="min-h-screen bg-background text-foreground overflow-x-hidden"> 
 
         { /* theme toogle */}
 
